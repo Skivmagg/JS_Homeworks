1d96bef8e1ca018a22a0c967271921b516695e23
AntonBuchynskyi <skivmagg@gmail.com>
2020-10-27 16:03:44 +0200

    Initial commit

---
+++ First_lesson/scripts.js
@@ -0,0 +1,224 @@
+//Завдання 1 - 6
+
+// let x = 'hello';
+// x = 'bye';
+// console.log(x);
+// alert(x);
+// document.write(x)
+//
+// let y = 'owu';
+// y = 'notowu';
+// console.log(y);
+// alert(y);
+// document.write('<br>', y)
+//
+// let z = 'com';
+// z = 'ua';
+// console.log(z);
+// alert(z);
+// document.write('<br>', z)
+//
+// let q = 1;
+// q = 555;
+// console.log(q);
+// alert(q);
+// document.write('<br>', q)
+//
+// let w = 10;
+// w = 666;
+// console.log(w);
+// alert(w);
+// document.write('<br>', w)
+//
+// let e = -999;
+// e = -2345;
+// console.log(e);
+// alert(e);
+// document.write('<br>', e)
+//
+// let r = true;
+// r = false;
+// console.log(r);
+// alert(r);
+// document.write('<br>', r)
+
+// const x = 5;
+// const y = 10;
+// const z = 15;
+// const q = 'Hello';
+// const w = 'My';
+// const e = 'World';
+//
+// console.log(x);
+// alert(x);
+// document.write(x)
+//
+// console.log(y);
+// alert(y);
+// document.write('<br>', y)
+//
+// console.log(z);
+// alert(z);
+// document.write('<br>', z)
+//
+// console.log(q);
+// alert(q);
+// document.write('<br>', q)
+//
+// console.log(w);
+// alert(w);
+// document.write('<br>', w)
+//
+// console.log(e);
+// alert(e);
+// document.write('<br>', e)
+
+// let firstNameME = prompt('What is your name?');
+// let secondNameME = prompt('What is your secondname');
+// let fatherNameME = prompt('What is your fathername');
+//
+// console.log(firstNameME);
+// alert(firstNameME);
+// document.write('<br>', firstNameME)
+//
+// console.log(secondNameME);
+// alert(secondNameME);
+// document.write('<br>', secondNameME)
+//
+// console.log(fatherNameME);
+// alert(fatherNameME);
+// document.write('<br>', fatherNameME)
+//
+// let person = (firstNameME + secondNameME + fatherNameME);
+// document.write('<br>', person)
+// console.log(person);
+//
+//
+// let firstNameMOM = prompt('What is your name?');
+// let secondNameMOM = prompt('What is your secondname');
+// let fatherNameMOM = prompt('What is your fathername');
+//
+// console.log(firstNameMOM);
+// alert(firstNameMOM);
+// document.write('<br>', firstNameMOM)
+//
+// console.log(secondNameMOM);
+// alert(secondNameMOM);
+// document.write('<br>', secondNameMOM)
+//
+// console.log(fatherNameMOM);
+// alert(fatherNameMOM);
+// document.write('<br>', fatherNameMOM)
+//
+// let personSecond = (firstNameMOM + secondNameMOM + fatherNameMOM);
+// document.write('<br>', personSecond)
+// console.log(personSecond);
+//
+//
+// let firstNameFATHER = prompt('What is your name?');
+// let secondNameFATHER = prompt('What is your secondname');
+// let fatherNameFATHER = prompt('What is your fathername');
+//
+// console.log(firstNameFATHER);
+// alert(firstNameFATHER);
+// document.write('<br>', firstNameFATHER)
+//
+// console.log(secondNameFATHER);
+// alert(secondNameFATHER);
+// document.write('<br>', secondNameFATHER)
+//
+// console.log(fatherNameFATHER);
+// alert(fatherNameFATHER);
+// document.write('<br>', fatherNameFATHER)
+//
+// let personThird = (firstNameFATHER + secondNameFATHER + fatherNameFATHER);
+// document.write('<br>', personThird)
+// console.log(personThird);
+
+
+//Завдання 7-8
+
+// let x = prompt('Write number');
+// let y = prompt('Write number');
+// let z = prompt('Write number');
+// let r = prompt('Write number');
+//
+// let result = parseInt(x) + parseInt(y) + parseInt(z) + parseInt(r);
+// console.log(result);
+
+
+
+//Завдання 9
+
+// let x = prompt('Write number');
+// let y = prompt('Write number');
+// let z = prompt('Write number');
+// let r = prompt('Write number');
+//
+// let result = parseFloat(x) + parseFloat(y) + parseFloat(z) + parseFloat(r);
+// console.log(result);
+
+
+
+//Завдання 10
+
+// let x = prompt('Write number');
+// let y = prompt('Write number');
+// let z = prompt('Write number');
+// let r = prompt('Write number');
+//
+// let result = Math.round(x) + Math.round(y) + Math.round(z) + Math.round(r);
+// console.log(result);
+
+
+
+//Завдання 11
+
+// let x = prompt('Write number');
+// let y = prompt('Write number');
+//
+// let resultX = parseInt(x);
+// let resultY = parseInt(y);
+// console.log(Math.pow(resultX,resultY));
+
+
+
+//Завдання 12
+
+// let a = 100;
+// let b = '100';
+// let c = true;
+// let d = undefined;
+//
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+// console.log(typeof d);
+
+
+
+
+// 5 < 6 -> true
+// 5 > 6 -> false
+// 5 >= 6 -> false
+// 5 === 6 -> false
+// 10 == 10 -> true
+// 10 <= 10 -> true
+// 10 > 10 -> false
+// 10 < 10 -> false
+// 10 != 10 -> false
+// 123 === '123' -> false
+// 123 == '123' -> true
+
+// console.log(132 > 100 && 45 < 12 ); //false
+// console.log(34 > 33 && 23 < 90 ); //true
+// console.log(99 > 100 && 45 > 12 ); //false
+// console.log(132 > 100 || 45 < 12 ); //true
+// console.log(111 > 11 || 45 < 111 ); //true
+// console.log((111 > 11 || 45 < 111) && (132 > 100 || 45 < 12) ); //true
+// console.log((111 > 11 || 45 < 56) || (132 > 100 || 45 < 12) ); //true
+// console.log((132 > 100 && 45 < 12 ) && (132 > 100 || 45 < 12 ) ); //false
+// console.log((111 > 11 || 45 < 111) || (99 > 100 && 45 > 12 ));// true
+
+
+
