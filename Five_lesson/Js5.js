810bee28875f10ceef64f13e2541339dfcb23738
AntonBuchynskyi <skivmagg@gmail.com>
2020-11-07 17:15:56 +0200

    Constructors done + Tax Bonus

--- Five_lesson/Js5.js
+++ Five_lesson/Js5.js
@@ -197,45 +197,142 @@
 // --Сторити об'єкт типу "принц" за допомоги функції конструктора з полями ім'я, вік, туфелька яку він знайшов, та функцію "пошук попелюшки"
 // -- функція повинна приймати масив попелюшок, та шукає ту котра йому підходить
 
-function King (name, age, bootSize){
-    this.name = name;
-    this.age = age;
-    this.bootSize = bootSize;
+// function King (name, age, bootSize){
+//     this.name = name;
+//     this.age = age;
+//     this.bootSize = bootSize;
+//     }
+//
+// let kingJ = new King('Anton', 26, 35)
+//
+//
+// function Girl (name, age, size){
+//     this.name = name;
+//     this.age = age;
+//     this.size = size;
+// }
+//
+// let girl1 = new Girl('Ira', 25, 41)
+// let girl2 = new Girl('Anya', 23, 38)
+// let girl3 = new Girl('Vara', 24, 38)
+// let girl4 = new Girl('Oksana', 22, 33)
+// let girl5 = new Girl('Malina', 21, 34)
+// let girl6 = new Girl('Olya', 27, 33)
+// let girl7 = new Girl('Tanya', 24, 35)
+// let girl8 = new Girl('Ivanna', 29, 38)
+// let girl9 = new Girl('Masha', 32, 39)
+// let girl10 = new Girl('Karina', 22, 34)
+//
+//
+// let allGirls = [girl1,girl2,girl3,girl4,girl5,girl6,girl7,girl8,girl9,girl10]
+//
+// function findGirl (array) {
+//     for (let i = 0; i < array.length; i++) {
+//         let girlElement = array[i];
+//         if (girlElement.size === kingJ.bootSize) {
+//             console.log(`Попелюшка яка повинна бути з принцом, це - ${girlElement.name}`)
+//         }
+//     }
+// }
+//     findGirl(allGirls);
+
+
+// BONUS
+//
+//
+// Дан массив целых чисел, найдите тот, который встречается нечетное количество раз.Всегда будет только одно целое число
+//     , которое встречается нечетное количество раз
+
+// Створити обєкт автомобіля з полями:
+//     Марка автомобля, потужність двигуна, власник, ціна, рік випуску.
+//     Власник автомобіля теж має бути обєкт, у якого є поля
+// Імя, вік, стаж водіння.
+//     Створити не менше 7 та не більше 20 машинок.
+//     Зробили половину автопарку ремонт мотору, що збільшить потужність автомобілів на 10% (переприсвоєння змінної потужності).
+// На відремонтовані автомобілі найняти нових водіїв (переприсвоїти змінну водій).
+// Для початку вкладіть всі наші створені автомобілі в масив cars.
+//     Далі необхідно рати кожну другу машинку (цикл з кроком в 2), та робити їй підвищення потужності двигуна на 10% та ціну на 5%
+// Після того зробити перевірку досвіду ВСІХ наших водіїв. Якщо досвід водія менший за 5 років, але його вік більший за 25, то необідно відправити його на курси підвищення кваліфікації, що збільшить йому досвід на 1 рік.
+//     Також спробуйте порахувати суму, яку потрібно потратити для покупки всіх цих авто в циклі
+
+class newCar {
+    constructor(marka,engPower,price,year,owner) {
+        this.marka = marka;
+        this.engPower = engPower;
+        this.price = price;
+        this.year = year;
+        this.owner = owner;
     }
-
-let kingJ = new King('Anton', 26, 35)
-
-
-function Girl (name, age, size){
-    this.name = name;
-    this.age = age;
-    this.size = size;
-}
-
-let girl1 = new Girl('Ira', 25, 41)
-let girl2 = new Girl('Anya', 23, 38)
-let girl3 = new Girl('Vara', 24, 38)
-let girl4 = new Girl('Oksana', 22, 33)
-let girl5 = new Girl('Malina', 21, 34)
-let girl6 = new Girl('Olya', 27, 33)
-let girl7 = new Girl('Tanya', 24, 35)
-let girl8 = new Girl('Ivanna', 29, 38)
-let girl9 = new Girl('Masha', 32, 39)
-let girl10 = new Girl('Karina', 22, 34)
-
-
-let allGirls = [girl1,girl2,girl3,girl4,girl5,girl6,girl7,girl8,girl9,girl10]
-
-function findGirl (array) {
-    for (let i = 0; i < array.length; i++) {
-        let girlElement = array[i];
-        if (girlElement.size === kingJ.bootSize) {
-            console.log(`Попелюшка яка повинна бути з принцом, це - ${girlElement.name}`)
+        engineRemont () {
+            this.engPower = Math.floor((this.engPower * 1.1));
         }
-    }
-}
-    findGirl(allGirls);
+        priceUp () {
+            this.price = Math.floor((this.price * 1.05));
+        }
+        newDriver (newDriverObject){
+        this.owner.name = newDriverObject.name;
+        this.owner.age = newDriverObject.age;
+        this.owner.drivingAge = newDriverObject.drivingAge;
+        }
+        training (){
+        this.owner.drivingAge += 1;
+        }
 
+}
+
+let car1 = new newCar('Reno',107,3400,2005,{name:'Anton', age: 26, drivingAge: 5});
+let car2 = new newCar('Mitsubisi',127,3200,2006,{name:'Vanya', age: 30, drivingAge: 2});
+let car3 = new newCar('Reno',157,3800,2008,{name:'Volodya', age: 42, drivingAge: 10});
+let car4 = new newCar('Mersedes',199,6400,2015,{name:'Andrey', age: 35, drivingAge: 12});
+let car5 = new newCar('BMW',237,8400,2016,{name:'Sergiy', age: 55, drivingAge: 25});
+let car6 = new newCar('Pejo',68,2400,2007,{name:'Ira', age: 26, drivingAge: 5});
+let car7 = new newCar('RolsRoys',407,17400,2013,{name:'Petro', age: 35, drivingAge: 12});
+let car8 = new newCar('Deowoe',134,5400,2009,{name:'Ivan', age: 24, drivingAge: 7});
+let car9 = new newCar('Reno',121,4400,2008,{name:'Vova', age: 26, drivingAge: 4});
+let car10 = new newCar('Reno',132,7400,2014,{name:'Jora', age: 29, drivingAge: 9});
+
+
+car10.engineRemont();
+car2.engineRemont();
+car4.engineRemont();
+car5.engineRemont();
+car8.engineRemont();
+car10.newDriver({name:'JoraNEW', age: 32, drivingAge: 14});
+car2.newDriver({name:'VanyaNEW', age: 30, drivingAge: 3});
+car4.newDriver({name:'AndreyNEW', age: 35, drivingAge: 4});
+car5.newDriver({name:'SergiyNEW', age: 26, drivingAge: 5});
+car8.newDriver({name:'IvanNEW', age: 24, drivingAge: 7});
+
+// Для початку вкладіть всі наші створені автомобілі в масив cars.
+//     Далі необхідно рати кожну другу машинку (цикл з кроком в 2), та робити їй підвищення потужності двигуна на 10% та ціну на 5%
+// Після того зробити перевірку досвіду ВСІХ наших водіїв. Якщо досвід водія менший за 5 років, але його вік більший за 25, то необідно відправити його на курси підвищення кваліфікації, що збільшить йому досвід на 1 рік.
+//     Також спробуйте порахувати суму, яку потрібно потратити для покупки всіх цих авто в циклі
+
+let cars = [car1,car2,car3,car4,car5,car6,car7,car8,car9,car10];
+
+
+for (let i = 0; i < cars.length; i+=2) {
+    cars[i].engineRemont();
+    cars[i].priceUp();
+
+}
+
+for (let i = 0; i < cars.length; i++) {
+    if (cars[i].owner.drivingAge < 5 && cars[i].owner.age > 25){
+        console.log(`Водій ${cars[i].owner.name} відправляється на підвищення кваліфікації` )
+        cars[i].training();
+    } else {
+        console.log(`Водій ${cars[i].owner.name} пройшов перевірку` )
+    }
+
+}
+let carsPrice = null;
+for (let i = 0; i < cars.length; i++) {
+  carsPrice = carsPrice + cars[i].price;
+}
+
+console.log(cars);
+console.log(carsPrice);
 
 
 
